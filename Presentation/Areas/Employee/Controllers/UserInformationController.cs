48aade763962fd473b714cd7e4c27b694bf8ed95
parsapanahpoor <80142438+parsapanahpoor@users.noreply.github.com>
2021-07-18 13:08:44 +0430

    Create UserInformationController For Employee Area

---
+++ Presentation/Areas/Employee/Controllers/UserInformationController.cs
@@ -0,0 +1,39 @@
+﻿using DataAccess.Design_Pattern.UnitOfWork;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Models.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Presentation.Areas.Employee.Controllers
+{
+    [Area("Employee")]
+    [Authorize(Roles = "Employee")]
+    public class UserInformationController : Controller
+    {
+        #region Constructor
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly IUnitOfWork _context;
+
+
+        public UserInformationController(UserManager<User> userManager,
+            SignInManager<User> signInManager, IUnitOfWork context
+            )
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+
+        }
+        #endregion
+
+        public IActionResult EmployeeDocument()
+        {
+            return View();
+        }
+    }
+}
